c7ab34ed161ccbfaaa6d99cf376416cf431a1040
Rivio98 <rivaldogjoni1998@gmail.com>
2024-10-09 17:06:13 +0200

    [UPD]Add relations to models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'slug'];
+    public static function generateSlug($title)
+    {
+        return Str::slug($title, '-');
+    }
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -16,4 +16,9 @@ class Project extends Model
     {
         return Str::slug($title, '-');
     }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
